c4cb1f830c67178faa216d8d1eab47693c9740ba
Asjad <asjad_2@hotmail.com>
2019-10-24 16:19:08 -0400

    made the mazeGame appear on the start game button so teammate can work on it

--- phase1/MyApplication/app/src/main/java/com/example/myapplication/GameActivity.java
+++ phase1/MyApplication/app/src/main/java/com/example/myapplication/GameActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.myapplication.WhackAMole.MoleActivity;
+
 public class GameActivity extends AppCompatActivity {
 
     @Override
@@ -21,5 +23,10 @@ public class GameActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TypeRacer.class);
         startActivity(intent);
 
+    }
+
+    public void play_Maze(View view) {
+        Intent intent = new Intent(this, MazeGame.class);
+        startActivity(intent);
     }
 }
