d651dadcf7e53054df38585b5e5744f94c473502
Michael <skyjoy.liu@mail.utoronto.ca>
2019-11-16 16:13:24 -0500

    "Created instruction page for WAM"

--- phase1/MyApplication/app/src/main/java/com/example/myapplication/WhackAMole/MoleInstructionActivity.java
+++ phase1/MyApplication/app/src/main/java/com/example/myapplication/WhackAMole/MoleInstructionActivity.java
@@ -2,20 +2,40 @@ package com.example.myapplication.WhackAMole;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.myapplication.R;
+import com.example.myapplication.User;
+import com.example.myapplication.UserManager;
+
+import static com.example.myapplication.MainActivity.USER;
 
 public class MoleInstructionActivity extends AppCompatActivity {
 
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mole_instruction);
+
+        Intent intent = getIntent();
+        User user_1 = (User) intent.getSerializableExtra(USER);
+        if (user_1 != null) {
+            setUser(user_1);
+        }
     }
 
     public void play_game(View v){
-        setContentView(R.layout.activity_mole);
+        user.setLoad_moles_stats("0");
+        UserManager.update_statistics(this, user);
+        Intent intent = new Intent(this, MoleInstructionActivity.class);
+        intent.putExtra(USER, user);
+        startActivity(intent);
+    }
+
+    private void setUser(User new_user) {
+        user = new_user;
     }
 }
