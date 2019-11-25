3caa4c3062507d1c9cb52a1582d06074db376396
allyaale <aalea.ally@mail.utoronto.ca>
2019-11-25 18:45:09 -0500

    Made it so that when a collectible is collected in the maze, the collectible and score statistics will be updated.
    Signed-off-by: Asjad <asjad_2@hotmail.com>

--- phase1/MyApplication/app/src/main/java/com/example/myapplication/Maze/Collectible.java
+++ phase1/MyApplication/app/src/main/java/com/example/myapplication/Maze/Collectible.java
@@ -4,10 +4,6 @@ package com.example.myapplication.Maze;
  * A collectible object in the maze game
  */
 public class Collectible extends Cell {
-    /**
-     * Whether this object was collected or not
-     */
-    private boolean collected;
 
     /**
      * points gained by collecting this objects
@@ -16,23 +12,11 @@ public class Collectible extends Cell {
 
     Collectible(int col, int row) {
         super(col, row);
-        collected = false;
         points = 1;
-    }
-
-    public boolean isCollected() {
-        return collected;
-    }
-
-    public void setCollected(boolean collected) {
-        this.collected = collected;
     }
 
     public int getPoints() {
         return points;
     }
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
 }
