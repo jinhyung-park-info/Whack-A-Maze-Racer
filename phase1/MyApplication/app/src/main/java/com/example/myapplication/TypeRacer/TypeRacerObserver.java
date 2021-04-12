653ab172db87b35751761759dedd7a247c40e798
Jinhyung Park <jh960426@naver.com>
2021-04-13 03:28:15 +0900

    Removed Unnecessary Files

--- phase1/MyApplication/app/src/main/java/com/example/myapplication/TypeRacer/TypeRacerObserver.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication.TypeRacer;
-
-public interface TypeRacerObserver {
-    public void update(int scores, int streaks, int life);
-}
--- phase1/MyApplication/app/src/main/java/com/example/myapplication/TypeRacer/TypeRacerSubject.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.TypeRacer;
-
-import com.example.myapplication.TypeRacer.TypeRacerObserver;
-
-public interface TypeRacerSubject {
-    public void registerObserver(TypeRacerObserver o);
-    public void unregisterObserver(TypeRacerObserver o);
-    public void notifyObservers();
-}
